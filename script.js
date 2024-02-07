cad5af48c8fb2d2d48461e441aa365d40a50b3a9
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-07 12:21:45 +0100

    Checking if is a palindrome

--- script.js
+++ script.js
@@ -3,10 +3,13 @@ const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
 const checkPalindrome = (input) => {
+    const originalInput = input;
+
     if (input === '') {
         alert('Please input a value');
         return;
     }
 
     const lowerCaseStr = input.replace('/[^A-Za-z0-9]/gi', '').toLowerCase(); // remove non alphanumeric characters
+    let resultMessage = `<strong>${originalInput}</strong> ${lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is a' : 'is not a'} palindrome`;
 }
