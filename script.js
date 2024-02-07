119c86246132a9678c6a63d6fc3080b354955ff0
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-07 12:00:04 +0100

    Checking that the user enters a value

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ const userInput = document.getElementById('text-input');
 const checkPalindromeBtn = document.getElementById('check-btn');
 const resultDiv = document.getElementById('result');
 
+const checkPalindrome = (input) => {
+    if (input === '') {
+        alert('Please input a value');
+        return;
+    }
+}
