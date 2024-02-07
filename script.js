968e55a4bfbc6b718880702ed4d9f5674ca58a88
Erik Sebastián de Erice <erikote2004@icloud.com>
2024-02-07 12:28:02 +0100

    Creating "p" tag to show the results

--- script.js
+++ script.js
@@ -11,5 +11,11 @@ const checkPalindrome = (input) => {
     }
 
     const lowerCaseStr = input.replace('/[^A-Za-z0-9]/gi', '').toLowerCase(); // remove non alphanumeric characters
-    let resultMessage = `<strong>${originalInput}</strong> ${lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is a' : 'is not a'} palindrome`;
+    let resultMessage = `<strong>${originalInput}</strong> ${lowerCaseStr === [...lowerCaseStr].reverse().join('') ? 'is' : 'is not'} a palindrome`;
+
+    const pTag = document.createElement('p');
+    pTag.className = 'user-input';
+    pTag.innerHTML = resultMessage;
+    resultDiv.appendChild(pTag);
+
 }
